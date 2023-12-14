1360200bc6b3657be7ea15a86772048825275fff
RichardPickman <drezzerock@gmail.com>
2023-12-14 15:38:32 +0100

    feat: destructure Navbar component and add animations

--- src/components/Navbar/ui/Links.tsx
+++ src/components/Navbar/ui/Links.tsx
@@ -1,54 +1,39 @@
-import { DarkThemeToggle } from 'flowbite-react';
 import { NavigationButton } from './Button';
 import { NavigationLink } from './Link';
 import { useAuthContext } from '@/context/AuthContext';
-import { ReactNode } from 'react';
 
-const Links = ({
-  direction = 'row',
-  className,
-}: {
-  direction?: 'row' | 'col';
-  className?: ReactNode;
-}) => {
+const Links = () => {
   const { user, logout } = useAuthContext();
-  const dir = {
-    row: 'flex-row items-center',
-    col: 'flex-col justify-center',
-  };
-
-  const cn = `flex ${dir[direction]} ${className}`;
 
   if (user.isAuthenticated) {
     return (
-      <div className={cn}>
+      <>
         <NavigationLink
-          dataTestId="nav-main-btn"
+          data-testid="nav-main-btn"
           href="/main"
           currentPath={'/'}
         >
           <NavigationButton text="Main" />
         </NavigationLink>
+
         <NavigationButton
           text="Sign out"
-          dataTestId="nav-signout-btn"
+          data-testid="nav-signout-btn"
           onClick={logout}
         />
-        <DarkThemeToggle data-testid="nav-light-dark-theme" />
-      </div>
+      </>
     );
   }
 
   return (
-    <div className={cn}>
+    <>
       <NavigationLink href="/login" currentPath={'/login'}>
-        <NavigationButton text="Sign In" dataTestId="nav-signin-btn" />
+        <NavigationButton text="Sign In" data-testid="nav-signin-btn" />
       </NavigationLink>
       <NavigationLink href="/login?signup=1" currentPath={'/login'}>
-        <NavigationButton text="Sign Up" dataTestId="nav-signup-btn" />
+        <NavigationButton text="Sign Up" data-testid="nav-signup-btn" />
       </NavigationLink>
-      <DarkThemeToggle data-testid="nav-light-dark-theme" />
-    </div>
+    </>
   );
 };
 
