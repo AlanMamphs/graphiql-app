3d1678aa89312ca902fa5c40640bf750cae4b5b9
RichardPickman <drezzerock@gmail.com>
2023-12-16 19:04:40 +0100

    feat: add sticky menu to nav bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,9 +6,16 @@ import {
   useMotionValueEvent,
   useScroll,
 } from 'framer-motion';
-import { navBarVariants } from './lib/variants';
+import Image from 'next/image';
+import {
+  verticalPrimaryVariants,
+  verticalSecondaryVariants,
+} from './lib/verticalSwap';
 import { Brand } from './ui/Brand';
 import { Links } from './ui/Links';
+import { Menu } from './ui/Menu';
+
+const HEADER_HEIGHT = 60;
 
 export const NavbarComponent = () => {
   const { stage, onOpen, onClose } = useStages('open');
@@ -16,30 +23,43 @@ export const NavbarComponent = () => {
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, 'change', (latest) => {
-    const pos = lastPosition.get();
-
-    pos < latest ? onClose() : onOpen();
+    latest > HEADER_HEIGHT ? onClose() : onOpen();
 
     lastPosition.set(latest);
   });
 
   return (
     <motion.header
-      className="sticky top-0 z-10"
-      variants={navBarVariants}
+      className="sticky top-0 z-10 flex flex-col h-16"
       animate={stage}
     >
-      <Navbar fluid rounded>
-        <Brand />
+      <motion.div variants={verticalPrimaryVariants} animate={stage}>
+        <Navbar fluid rounded>
+          <Brand />
 
-        <Navbar.Collapse>
-          <Links />
+          <Navbar.Collapse>
+            <Links />
 
-          <li>
-            <DarkThemeToggle data-testid="nav-light-dark-theme" />
-          </li>
-        </Navbar.Collapse>
-      </Navbar>
+            <li>
+              <DarkThemeToggle data-testid="nav-light-dark-theme" />
+            </li>
+          </Navbar.Collapse>
+        </Navbar>
+      </motion.div>
+      <motion.div
+        data-testid="nav-secondary-menu"
+        className="px-2 py-2.5 sm:px-4 rounded"
+        initial={{ opacity: 0 }}
+        variants={verticalSecondaryVariants}
+        animate={stage}
+      >
+        <div className="flex justify-between">
+          <motion.div className="w-10 aspect-square relative">
+            <Image alt="Logo" src="/favicon.png" fill />
+          </motion.div>
+          <Menu />
+        </div>
+      </motion.div>
     </motion.header>
   );
 };
