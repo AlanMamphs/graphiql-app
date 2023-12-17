3c7b40359d5990b06bcc39d1fce6b9ef77775007
RichardPickman <drezzerock@gmail.com>
2023-12-17 23:38:46 +0100

    refactor: destructure header for readability

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,6 @@
 import { useStages } from '@/hooks/useStages';
 import { DarkThemeToggle, Navbar } from 'flowbite-react';
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-} from 'framer-motion';
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 import Image from 'next/image';
 import {
   verticalPrimaryVariants,
@@ -17,15 +12,48 @@ import { Menu } from './ui/Menu';
 
 const HEADER_HEIGHT = 60;
 
+interface HeaderProps {
+  stage: 'open' | 'closed';
+}
+
+const DefaultHeader = ({ stage }: HeaderProps) => (
+  <motion.div variants={verticalPrimaryVariants} animate={stage}>
+    <Navbar fluid rounded>
+      <Brand />
+      <Navbar.Collapse>
+        <Links />
+
+        <li>
+          <DarkThemeToggle data-testid="nav-light-dark-theme" />
+        </li>
+      </Navbar.Collapse>
+    </Navbar>
+  </motion.div>
+);
+
+const FloatHeader = ({ stage }: HeaderProps) => (
+  <motion.div
+    data-testid="nav-secondary-menu"
+    className="px-2 py-2.5 sm:px-4 rounded"
+    initial={{ opacity: 0 }}
+    variants={verticalSecondaryVariants}
+    animate={stage}
+  >
+    <div className="flex justify-between">
+      <motion.div className="w-10 aspect-square relative">
+        <Image alt="Logo" src="/favicon.png" fill />
+      </motion.div>
+      <Menu />
+    </div>
+  </motion.div>
+);
+
 export const NavbarComponent = () => {
   const { stage, onOpen, onClose } = useStages('open');
-  const lastPosition = useMotionValue(0);
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, 'change', (latest) => {
     latest > HEADER_HEIGHT ? onClose() : onOpen();
-
-    lastPosition.set(latest);
   });
 
   return (
@@ -33,33 +61,8 @@ export const NavbarComponent = () => {
       className="sticky top-0 z-10 flex flex-col h-16"
       animate={stage}
     >
-      <motion.div variants={verticalPrimaryVariants} animate={stage}>
-        <Navbar fluid rounded>
-          <Brand />
-
-          <Navbar.Collapse>
-            <Links />
-
-            <li>
-              <DarkThemeToggle data-testid="nav-light-dark-theme" />
-            </li>
-          </Navbar.Collapse>
-        </Navbar>
-      </motion.div>
-      <motion.div
-        data-testid="nav-secondary-menu"
-        className="px-2 py-2.5 sm:px-4 rounded"
-        initial={{ opacity: 0 }}
-        variants={verticalSecondaryVariants}
-        animate={stage}
-      >
-        <div className="flex justify-between">
-          <motion.div className="w-10 aspect-square relative">
-            <Image alt="Logo" src="/favicon.png" fill />
-          </motion.div>
-          <Menu />
-        </div>
-      </motion.div>
+      <DefaultHeader stage={stage} />
+      <FloatHeader stage={stage} />
     </motion.header>
   );
 };
