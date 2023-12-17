8b7607d4608606785dd844059ec23e3aaa46399a
RichardPickman <drezzerock@gmail.com>
2023-12-17 23:27:59 +0100

    refactor: remove unnecessary wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,49 +9,47 @@ export const HomePageContent: NextPage = function () {
       data-testid="welcome-banner"
       className="flex justify-center w-full h-full overflow-y-auto mx-auto"
     >
-      <Card className="m-4">
-        <div className="flex flex-col justify-between p-4 shadow-sm lg:max-w-7xl">
-          <div className="flex">
-            <Link
-              href="https://github.com/rolling-scopes-school/tasks/blob/master/react/modules/graphiql.md"
-              className="flex items-center gap-2"
-            >
-              <Image alt="Logo" height="48" src="/favicon.png" width="48" />
-              <span className="self-center whitespace-nowrap text-lg font-semibold md:pr-6">
-                GraphiQL
-              </span>
-            </Link>
-            <div className="flex flex-col justify-center h-full">
-              <h2 className="text-base font-semibold text-gray-900 dark:text-white">
-                Welcome to the GraphiQL
-              </h2>
+      <Card className="flex flex-col justify-between p-4 shadow-sm lg:max-w-7xl m-4 h-[550px]">
+        <div className="flex">
+          <Link
+            href="https://github.com/rolling-scopes-school/tasks/blob/master/react/modules/graphiql.md"
+            className="flex items-center gap-2"
+          >
+            <Image alt="Logo" height="48" src="/favicon.png" width="48" />
+            <span className="self-center whitespace-nowrap text-lg font-semibold md:pr-6">
+              GraphiQL
+            </span>
+          </Link>
+          <div className="flex flex-col justify-center h-full">
+            <h2 className="text-base font-semibold text-gray-900 dark:text-white">
+              Welcome to the GraphiQL
+            </h2>
 
-              <p className="flex items-center text-sm font-normal w-full max-w-lg whitespace-break-spaces">
-                GraphiQL is a playground/IDE for graphQL requests. This project
-                is part of Rolling Scopes School React Course. The Rolling
-                Scopes is an independent international community of developers
-                (mainly JavaScript / Front-end / iOS / Android), organized in
-                2013. Currently, many developers around the world know that we
-                participate in our events and activities. You can find task
-                description and requirements
-                <Link
-                  className="contents font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  href="https://github.com/rolling-scopes-school/tasks/blob/master/react/modules/graphiql.md"
-                  target="_blank"
-                >
-                  {' '}
-                  here
-                </Link>
-              </p>
-            </div>
-            <div className="flex flex-shrink-0 items-center gap-2">
-              <Link href="/login?signup=0">
-                <Button data-testid="welcome-signin-btn">Sign In</Button>
+            <p className="flex items-center text-sm font-normal w-full max-w-lg whitespace-break-spaces">
+              GraphiQL is a playground/IDE for graphQL requests. This project is
+              part of Rolling Scopes School React Course. The Rolling Scopes is
+              an independent international community of developers (mainly
+              JavaScript / Front-end / iOS / Android), organized in 2013.
+              Currently, many developers around the world know that we
+              participate in our events and activities. You can find task
+              description and requirements
+              <Link
+                className="contents font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                href="https://github.com/rolling-scopes-school/tasks/blob/master/react/modules/graphiql.md"
+                target="_blank"
+              >
+                {' '}
+                here
               </Link>
-              <Link href="/login?signup=1">
-                <Button data-testid="welcome-signup-btn">Sign up</Button>
-              </Link>
-            </div>
+            </p>
+          </div>
+          <div className="flex flex-shrink-0 items-center gap-2">
+            <Link href="/login?signup=0">
+              <Button data-testid="welcome-signin-btn">Sign In</Button>
+            </Link>
+            <Link href="/login?signup=1">
+              <Button data-testid="welcome-signup-btn">Sign up</Button>
+            </Link>
           </div>
         </div>
       </Card>
