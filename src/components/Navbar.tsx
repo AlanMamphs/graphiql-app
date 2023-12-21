bb14ab6c98fffce9d4179dfe35dad3184f8e9961
RichardPickman <drezzerock@gmail.com>
2023-12-21 12:16:13 +0100

    refactor: add flexbox for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,33 +11,42 @@ export const NavbarComponent: FC<Record<string, never>> = function () {
   const { user, logout } = useAuthContext();
 
   return (
-    <header className="sticky top-0 z-10">
-      <Link className="flex gap-3" data-testid="nav-brand" href="/">
-        <Image alt="Logo" height="24" src="/favicon.png" width="24" />
+    <header className="sticky top-0 z-10 flex justify-between py-2 px-2">
+      <Link className="w-fit flex gap-3" data-testid="nav-brand" href="/">
+        <Image alt="Logo" width="40" height="40" src="/favicon.png" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           GraphiQL Editor
         </span>
       </Link>
       {user.isAuthenticated ? (
-        <>
+        <div className="flex justify-center items-center gap-2">
           <Link data-testid="nav-main-btn" href="/main">
-            <Button>Main</Button>
+            <Button variant="outline">Main</Button>
           </Link>
-          <Button data-testid="nav-signout-btn" onClick={logout}>
+          <Button
+            variant="outline"
+            data-testid="nav-signout-btn"
+            onClick={logout}
+          >
             Sign Out
           </Button>
-        </>
+          <ModeToggle />
+        </div>
       ) : (
-        <>
+        <div className="flex justify-center items-center gap-2">
           <Link href="/login">
-            <Button data-testid="nav-signin-btn">Sign In</Button>
+            <Button variant="outline" data-testid="nav-signin-btn">
+              Sign In
+            </Button>
           </Link>
           <Link href="/login?signup=1">
-            <Button data-testid="nav-signup-btn">Sign Up</Button>
+            <Button variant="outline" data-testid="nav-signup-btn">
+              Sign Up
+            </Button>
           </Link>
-        </>
+          <ModeToggle />
+        </div>
       )}
-      <ModeToggle />
     </header>
   );
 };
