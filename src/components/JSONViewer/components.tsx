0295aef7657e77c32d0325b5e63177a4d156e2fe
RichardPickman <drezzerock@gmail.com>
2023-12-25 16:46:43 +0100

    refactor: make a few  changes

--- src/components/JSONViewer/components.tsx
+++ src/components/JSONViewer/components.tsx
@@ -4,13 +4,12 @@ import { getJsonNodes } from './lib';
 interface KeyProps {
   keyString: string;
   value: string;
-  offset: number;
 }
 
 export const KeyWrapper = memo(
   ({ keyString, children }: { keyString: string; children: ReactNode }) => (
     <span>
-      <span className="text-amber-300">&quot;{keyString}&quot;</span>:{' '}
+      <span className="text-orange-400">&quot;{keyString}&quot;</span>:{' '}
       {children}
     </span>
   )
@@ -20,7 +19,7 @@ KeyWrapper.displayName = 'KeyWrapper';
 
 export const StringComponent = ({ keyString, value }: KeyProps) => (
   <KeyWrapper keyString={keyString}>
-    <span className="text-green-300">
+    <span className="text-emerald-400">
       &quot;{value.replace('\n', '').replace('\r', '')}&quot;
     </span>
     , <br />
@@ -30,53 +29,49 @@ export const StringComponent = ({ keyString, value }: KeyProps) => (
 interface NumberProps {
   keyString: string;
   value: number;
-  offset: number;
 }
 
 export const NumberComponent = ({ keyString, value }: NumberProps) => (
   <KeyWrapper keyString={keyString}>
-    <span className="text-blue-300">{value}</span>, <br />
+    <span className="text-blue-400">{value}</span>, <br />
   </KeyWrapper>
 );
 
 interface BooleanProps {
   keyString: string;
   value: boolean;
-  offset: number;
 }
 
 export const BooleanComponent = ({ keyString, value }: BooleanProps) => (
   <KeyWrapper keyString={keyString}>
-    <span className="text-blue-300">{String(value)}</span>, <br />
+    <span className="text-violet-400">{String(value)}</span>, <br />
   </KeyWrapper>
 );
 
 interface ObjectProps {
   keyString: string;
   value: Record<string, unknown>;
-  offset: number;
 }
 
-export const ObjectComponent = ({ keyString, value }: ObjectProps) => (
-  <KeyWrapper keyString={keyString}>
-    <span className="text-blue-300">{`{`}</span> <br />
-    <div className="pl-10">{getJsonNodes(value)}</div>
-    <span className="text-blue-300">{`}`}</span>, <br />
-  </KeyWrapper>
-);
+export const ObjectComponent = ({ keyString, value }: ObjectProps) => {
+  return (
+    <KeyWrapper keyString={keyString}>
+      <span className="text-slate-400">{`{`}</span> <br />
+      <div className="pl-5">{getJsonNodes(value)}</div>
+      <span className="text-slate-400">{`}`}</span>, <br />
+    </KeyWrapper>
+  );
+};
 
 interface ArrayProps {
   keyString: string;
   value: object | null;
-  offset: number;
 }
 
 export const ArrayComponent = ({ keyString, value }: ArrayProps) => (
   <KeyWrapper keyString={keyString}>
-    <span className="text-blue-300">{`[`}</span> <br />
-    <div className="pl-10">
-      {getJsonNodes(value as Record<string, unknown>)}
-    </div>
-    <span className="text-blue-300">{`]`}</span>, <br />
+    <span className="text-slate-400">{`[`}</span> <br />
+    <div className="pl-5">{getJsonNodes(value as Record<string, unknown>)}</div>
+    <span className="text-slate-400">{`]`}</span>, <br />
   </KeyWrapper>
 );
--- src/components/JSONViewer/index.tsx
+++ src/components/JSONViewer/index.tsx
@@ -1,35 +1,33 @@
 import { getJsonNodes } from './lib';
 
-export const JsonViewer = () => {
-  const test_obj = {
-    index: 0,
-    isActive: true,
-    balance: '$3,206.73',
-    picture: 'http://placehold.it/32x32',
-    age: 36,
-    eyeColor: 'brown',
-    name: 'Cleo Stokes',
-    latitude: -82.085163,
-    longitude: 48.691609,
-    friends: [
-      {
-        id: 0,
-        name: 'Danielle Hill',
-      },
-      {
-        id: 1,
-        name: 'Mercer Boyle',
-      },
-      {
-        id: 2,
-        name: 'Hutchinson Warner',
-      },
-    ],
-  };
-
-  return (
-    <div className="flex flex-wrap overflow-auto">
-      <pre>{getJsonNodes(test_obj)}</pre>
-    </div>
-  );
+const test_obj = {
+  index: 0,
+  isActive: true,
+  balance: '$3,206.73',
+  picture: 'http://placehold.it/32x32',
+  age: 36,
+  eyeColor: 'brown',
+  name: 'Cleo Stokes',
+  latitude: -82.085163,
+  longitude: 48.691609,
+  friends: [
+    {
+      id: 0,
+      name: 'Danielle Hill',
+    },
+    {
+      id: 1,
+      name: 'Mercer Boyle',
+    },
+    {
+      id: 2,
+      name: 'Hutchinson Warner',
+    },
+  ],
 };
+
+export const JsonViewer = () => (
+  <div className="flex flex-wrap overflow-auto">
+    <pre>{getJsonNodes(test_obj)}</pre>
+  </div>
+);
