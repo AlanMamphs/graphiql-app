f0029ef1b12728284b932b66699f5e67eb9c7f23
RichardPickman <drezzerock@gmail.com>
2024-01-02 12:20:42 +0100

    feat: update the markdown of a JSONViewer

--- src/components/JSONViewer/components.tsx
+++ src/components/JSONViewer/components.tsx
@@ -14,12 +14,17 @@ const Key = ({ text }: { text: string }) => (
   <div className="text-key">&quot;{text}&quot;:</div>
 );
 
-interface ViewProps {
+interface BlockProps {
   keyString: string;
-  value: string | Record<string, unknown>;
+  value: Record<string, unknown>;
 }
 
-export const InlineView = ({ keyString, value }: ViewProps) => (
+interface InlineProps {
+  keyString: string;
+  value: string | number | null;
+}
+
+export const InlineView = ({ keyString, value }: InlineProps) => (
   <div className="flex gap-2 items-start">
     <Key text={keyString} />
     <div className={getClassNameByValueType(value)}>
@@ -28,7 +33,7 @@ export const InlineView = ({ keyString, value }: ViewProps) => (
   </div>
 );
 
-export const BlockView = ({ keyString, value }: ViewProps) => (
+export const BlockView = ({ keyString, value }: BlockProps) => (
   <div className="flex flex-col gap-1 items-start">
     <div className="flex gap-2">
       <Key text={keyString} />
@@ -38,7 +43,10 @@ export const BlockView = ({ keyString, value }: ViewProps) => (
       <div className="pl-5">
         <JsonViewer obj={value as Record<string, unknown>} />
       </div>
-      <p>{Array.isArray(value) ? '],' : '},'}</p>
+    </div>
+    <div className="flex">
+      <p className="text-start">{Array.isArray(value) ? ']' : '}'}</p>
+      {','}
     </div>
   </div>
 );
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,14 +1,37 @@
+import { JsonViewer } from '@/components/JSONViewer';
 import { NextPageContext } from 'next';
-import { getSession, useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+
+const obj = {
+  string: 'This is a string',
+  number: 42,
+  boolean: true,
+  nullValue: null,
+  undefinedValue: undefined,
+  array: ['element1', 'element2'],
+  object: {
+    key: 'value',
+    value: {
+      object: { key: 'value' },
+      array: ['element1', 'element2'],
+      undefinedValue: undefined,
+      nullValue: null,
+      boolean: true,
+      number: 42,
+      string: 'This is a string',
+    },
+  },
+};
 
 const Main = () => {
-  const { data } = useSession();
+  const validatedObject = JSON.parse(JSON.stringify(obj));
+
   return (
     <div
       data-testid="main-page"
       className="text-center bg-gray-50 my-10 dark:bg-gray-900 "
     >
-      Main Page {data?.user?.email}
+      <JsonViewer obj={validatedObject} />
     </div>
   );
 };
