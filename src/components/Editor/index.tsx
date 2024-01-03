99d151afd9566101fab1c949e2459df7650411d4
RichardPickman <drezzerock@gmail.com>
2024-01-03 15:39:03 +0100

    feat: add editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,84 +1,62 @@
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 
-export const QueryPrettified = ({
-  text,
-  paddedText,
-}: {
-  text: string;
-  paddedText: string;
-}) => {
-  return (
-    <div>
-      <div className="flex gap-2 items-center">{text}</div>
-      <div className="pl-2 empty:hidden">
-        <QueryPrettified text={text} paddedText={paddedText} />
-      </div>
-    </div>
-  );
-};
-
-const getItems = (text: string) => {
-  const index = text.indexOf('\n');
-  const textWithFirstNewLine = text
-    .substring(0, index)
-    .concat(' ')
-    .concat(text.substring(index + 1));
-  const arr = textWithFirstNewLine.split('\n');
-
-  return {
-    header: arr[0],
-    children: arr[1],
-  };
-};
-
-const HighlightedText = ({
-  value,
-  nesting,
-}: {
-  value: string;
-  nesting: number;
-}) => {
-  const [children, setChildren] = useState<string>('');
-
-  const formattedHeader = value.split(' ').map((item, index) => {
-    const systemWords = ['include', 'type', 'const', 'asd'];
-
-    if (systemWords.includes(item)) {
-      return (
-        <span key={item + index + nesting} className="text-red-400">
-          {item}{' '}
-        </span>
-      );
-    }
-
+const getFormattedJSXes = (text: string) => {
+  const arr = text.split(' ').map((item, index) => {
     const specialChars = ['{', '[', ']', '}', ',', '.', ':', ';'];
 
     if (specialChars.includes(item)) {
       return (
-        <span key={item + index + nesting} className="text-neutral-100">
+        <span key={item + index} className="text-neutral-100">
           {item}{' '}
         </span>
       );
     }
 
     return (
-      <span key={item + index + nesting} className="text-blue-100">
+      <span key={item + index} className="text-number">
         {item}{' '}
       </span>
     );
   });
 
-  if (value.includes('\n')) {
-    setChildren(value.split('\n').slice(1).join(''));
-  }
+  return arr;
+};
+
+const HighlightedText = ({ value }: { value: string }) => {
+  const [text, setText] = useState<string>('');
+  const [children, setChildren] = useState<string | null>(null);
+
+  useEffect(() => {
+    const prettifyRow = (label: string) => {
+      if (label.length > 80) {
+        setText(label + '\n');
+      }
+
+      setText(label);
+    };
+
+    if (value.includes('\n')) {
+      const arr = value.split('\n');
+
+      prettifyRow(arr[0]);
+      setChildren(arr.slice(1).join('\n'));
+    } else {
+      prettifyRow(value);
+    }
+  }, [value]);
+
+  const formattedHeader = getFormattedJSXes(text);
 
   return (
-    <div className="flex flex-col">
-      <div>{formattedHeader}</div>
-      <div className={`pl-${nesting + 2}`}>
-        <HighlightedText value={children} nesting={nesting} />
-      </div>
-    </div>
+    <>
+      {formattedHeader}
+      {children && (
+        <>
+          <br />
+          <HighlightedText value={children} />
+        </>
+      )}
+    </>
   );
 };
 
@@ -87,20 +65,50 @@ export const Editor = () => {
   const [text, setText] = useState('');
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    event.preventDefault();
-
     setText(event.target.value);
+  };
+
+  const onKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (ref.current) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        const posStart = event.currentTarget.selectionStart;
+        const start = event.currentTarget.value.slice(0, posStart);
+        const rest = event.currentTarget.value.slice(posStart);
+
+        setText(start + '\n    ' + rest);
+
+        ref.current.value = start + '\n    ' + rest;
+
+        ref.current.selectionStart = posStart + 5;
+        ref.current.selectionEnd = posStart + 5;
+      }
+
+      if (event.key === 'Tab') {
+        event.preventDefault();
+        const posStart = event.currentTarget.selectionStart;
+        const start = event.currentTarget.value.slice(0, posStart);
+        const rest = event.currentTarget.value.slice(posStart);
+
+        setText(start + '    ' + rest);
+
+        ref.current.value = start + '    ' + rest;
+        ref.current.selectionStart = posStart + 4;
+        ref.current.selectionEnd = posStart + 4;
+      }
+    }
   };
 
   return (
     <div className="relative w-full h-full">
-      <pre className="absolute inset-0 w-full h-full text-left">
-        <HighlightedText value={text} nesting={0} />
+      <pre className="absolute inset-0 text-blue w-full h-full">
+        <HighlightedText key={text} value={text || ''} />
       </pre>
       <textarea
         ref={ref}
         onChange={handleChange}
-        className="absolute text-blue w-full h-full outline-none inset-0 bg-transparent text-transparent caret-slate-100"
+        onKeyDown={onKeyDown}
+        className="absolute w-full h-full mb-auto overflow-auto outline-none bg-transparent text-transparent caret-slate-100"
       />
     </div>
   );
