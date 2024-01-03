96ec4d80bd34002f4e52fd02020a3624f4307931
RichardPickman <drezzerock@gmail.com>
2024-01-03 15:38:12 +0100

    refactor: refactor Main page a bit

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,3 +1,4 @@
+import { Editor } from '@/components/Editor';
 import { JsonViewer } from '@/components/JSONViewer';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
@@ -13,7 +14,7 @@ const obj = {
     key: 'value',
     value: {
       object1: { key: 'value' },
-      array2: ['element1', 'element2'],
+      array2: [{ key: 'value' }, { key: 'value' }, 'element2'],
       undefinedValue3: undefined,
       nullValue4: null,
       boolean5: true,
@@ -25,13 +26,16 @@ const obj = {
 
 const Main = () => {
   const validatedObject = JSON.parse(JSON.stringify(obj));
+  const className = 'w-full bg-gray-50 py-2 px-2 dark:bg-gray-900';
 
   return (
-    <div
-      data-testid="main-page"
-      className="text-center bg-gray-50 my-2 mx-2 py-2 px-2 dark:bg-gray-900"
-    >
-      <JsonViewer obj={validatedObject} />
+    <div className="flex gap-2">
+      <div className={className}>
+        <Editor />
+      </div>
+      <div data-testid="main-page" className={className}>
+        <JsonViewer obj={validatedObject} />
+      </div>
     </div>
   );
 };
